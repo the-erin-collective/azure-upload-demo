f28e0f6cdbc660a9f1979b9d16a94b0e1a203fe1
erin <fix.society.github@gmail.com>
2022-05-30 23:43:09 +0100

    add filecontextcore

---
+++ server/Data/Context.cs
@@ -0,0 +1,21 @@
+﻿using azure_upload_demo_server.Models;
+using FileContextCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace azure_upload_demo_server.Data
+{
+  public class Context : DbContext
+  {
+    public DbSet<Document> Documents { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      //Default: JSON-Serializer
+      optionsBuilder.UseFileContextDatabase();
+    }
+  }
+}
--- server/Startup.cs
+++ server/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using FileContextCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using azure_upload_demo_server.Data;
 
 namespace azure_upload_demo_server
 {
@@ -26,6 +28,7 @@ namespace azure_upload_demo_server
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
+      services.AddDbContext<Context>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
