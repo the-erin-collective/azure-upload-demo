6825c33ff039b019b0cc5501c03e86e853f0e4a7
erin <fix.society.github@gmail.com>
2022-05-30 23:26:58 +0100

    add server project from template

---
+++ server/Controllers/DocumentController.cs
@@ -0,0 +1,47 @@
+﻿using azure_upload_demo_server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace azure_upload_demo_server.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class DocumentController : ControllerBase
+  {
+    private readonly ILogger<DocumentController> _logger;
+
+    public DocumentController(ILogger<DocumentController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public string Get()
+    {
+      return "azure-upload-demo server is online.";
+    }
+
+    [HttpPost]
+    public IEnumerable<Document> All()
+    {
+      var documents = new List<Document>();
+      return documents;
+    }
+
+    [HttpPost]
+    public Document Upload(Document document)
+    {
+      return document;
+    }
+
+    [HttpPost]
+    public Document Download(Guid documentId)
+    {
+      return new Document();
+    }
+  }
+}
---
+++ server/Models/Document.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace azure_upload_demo_server.Models
+{
+  public class Document
+  {
+    public Guid Id { get; set; }
+    public DateTimeOffset DateCreated { get; set; }
+    public DateTimeOffset DateLastModified { get; set; }
+    public string Filename { get; set; }
+  }
+}
